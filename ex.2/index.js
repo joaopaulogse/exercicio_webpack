be6fb02aedf3cb7901bd51502ee0ff4c13d4154a
Joao Paulo <joaopaulogse@gmail.com>
2017-09-14 20:38:48 -0300

    exercicios

---
+++ ex.2/index.js
@@ -0,0 +1,4 @@
+import Pessoa from './pessoa';
+
+let pessoa = new Pessoa("joão");
+console.log(pessoa.toString());
---
+++ ex/index.js
@@ -0,0 +1,12 @@
+import './estilo.css'
+import React, {Component, DOM} from 'react'
+
+class App extends Component{
+    render(){
+        return <h1>Olá</h1>
+
+    }
+}
+
+
+console.log("Funcionou!")
